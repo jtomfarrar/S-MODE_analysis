d52ec01c6c2c5231e2b3513c6dac9f802b9cb137
jtomfarrar <44488331+jtomfarrar@users.noreply.github.com>
2021-11-15 12:59:58 -0500

    SST_HF_radar_plot added
    SST_HF_radar_plot uses sst_map_SMODE function

--- code/functions.py
+++ code/functions.py
@@ -71,3 +71,117 @@ def get_current_position(platform_str,**kwargs):
     
     return(platform)
 
+def sst_map_SMODE(url,zoom,V,time_window):
+    """
+    Plot map of SST for S-MODE region.
+
+    Parameters
+    ----------
+    url : URL for netcdf file with SST image (like from smode.whoi.edu)
+    zoom : Int, 0 to 4
+        zoom level for plots, larger value gives closer view, 
+            zoom = 0: #wide view of S-MODE ops area and San Francisco
+            zoom = 1: #centered on S-MODE ops area, but shows San Francisco
+            zoom = 2: #tight on S-MODE ops area
+            zoom = 3: #zoom on eastern part of ops area
+    V : list with 2 elements (e.g., [12, 16])
+        max and min of color range
+    time_window : float
+        number of hours to average HF radar data around time of SST image
+    
+    Returns
+    -------
+    None.
+
+    """
+    import numpy as np
+    import xarray as xr
+    import matplotlib.pyplot as plt
+    import matplotlib
+    import copy
+    import cartopy.crs as ccrs                   # import projections
+    import cartopy
+    import datetime
+    
+    if zoom == 0: #wide view of S-MODE ops area and San Francisco
+        xmin, xmax = (-126,-121)
+        ymin, ymax = (36, 39)
+        zoom_str='_wide'
+    elif zoom == 1: #centered on S-MODE ops area, but shows San Francisco
+        xmin, xmax = (-126,-122)
+        ymin, ymax = (36, 39)
+        zoom_str='_zoom1'
+    elif zoom == 2: #tight on S-MODE ops area
+        xmin, xmax = (-125.5,-123)
+        ymin, ymax = (36.5,38)
+        zoom_str='_zoom2'
+    elif zoom == 3: #zoom on eastern part of ops area
+        xmin, xmax = (-124.75,-122.5)
+        ymin, ymax = (36.3,38)
+        zoom_str='zoom3'
+    elif zoom == 4: #not yet determined
+        xmin, xmax = (-126,-122)
+        ymin, ymax = (36, 39)
+        zoom_str='_zoom4'
+    
+    #Set color scale range (define V with input file)
+    levels = np.linspace(V[0],V[1],21)
+    sst = xr.open_dataset(url)
+    
+    plt.figure()
+    ax = plt.axes(projection = ccrs.PlateCarree(central_longitude=200))  # Orthographic
+    extent = [xmin, xmax, ymin, ymax]
+    ax.set_extent(extent, crs=ccrs.PlateCarree())
+    day_str=sst.time.dt.strftime("%Y-%m-%d %H:%M").values[0]
+    day_str2=sst.time.dt.strftime("%Y-%m-%d").values[0]
+    #plt.set_cmap(cmap=plt.get_cmap('nipy_spectral'))
+    plt.set_cmap(cmap=plt.get_cmap('turbo'))
+    cmap = copy.copy(matplotlib.cm.get_cmap("turbo"))
+    cmap.set_bad(color='white')
+    gl = ax.gridlines(draw_labels=True, dms=True, x_inline=False, y_inline=False, alpha=0.5, linestyle='--')
+    gl.top_labels = False
+    gl.ylabels_right = False
+    
+    ax.coastlines()
+    ax.add_feature(cartopy.feature.LAND, zorder=3, facecolor=[.6,.6,.6], edgecolor='black')
+    arr = np.ma.array(sst.sea_surface_temperature, mask=(sst.quality_level == 1))
+    cs = ax.pcolormesh(sst.lon,sst.lat,np.squeeze(arr)-273.15, vmin=levels[0], vmax=levels[-1], transform=ccrs.PlateCarree())
+    cb = plt.colorbar(cs,fraction = 0.022,extend='both')
+    cb.set_label('SST [$\circ$C]',fontsize = 10)
+    plot_ops_area(ax,transform=ccrs.PlateCarree(),color='w')
+
+    ################################################
+    ## Now read in HF radar for appropriate time
+    # This could easily be spun off into a separrate function
+    center_timeDT = datetime.datetime.strptime(str(sst.time[0].values),'%Y-%m-%d %H:%M:%S') #time of SST image
+    # Choose time interval
+    time_delta=datetime.timedelta(hours=time_window/2)
+    
+    startTimeDT=center_timeDT-time_delta
+    endTimeDT=center_timeDT+time_delta
+    url = 'http://hfrnet-tds.ucsd.edu/thredds/dodsC/HFR/USWC/6km/hourly/RTV/HFRADAR_US_West_Coast_6km_Resolution_Hourly_RTV_best.ncd'
+
+    ds=xr.open_dataset(url,drop_variables=['hdop','dopx','dopy',]).sel(time=slice(startTimeDT,endTimeDT))
+    nanmask = np.isnan(ds.u).sum('time')>time_window/4
+    mask = np.where(nanmask.values,np.nan,0)
+    
+    # Try an experiment to plot velocity relative to a particular spot
+    vel_offset = False
+    if vel_offset:
+        x00 = -124.18
+        y00 = 37.3
+        u00 = ds.u.sel(lon=x00, lat=y00, method='nearest').mean('time').values
+        v00 = ds.v.sel(lon=x00, lat=y00, method='nearest').mean('time').values
+    else:
+        u00 = 0
+        v00 = 0
+
+    ax.quiver(ds.lon.values, ds.lat.values, mask+ds.u.mean('time').values-u00, ds.v.mean('time').values-v00,  scale=3, transform=ccrs.PlateCarree())
+    x0 = -122.8 
+    y0 = 37
+    ax.quiver(np.array([x0]), np.array([y0]), np.array([0.25/np.sqrt(2)]), np.array([0.25/np.sqrt(2)]), color='w', scale=3, transform=ccrs.PlateCarree())
+    ax.text(x0+3/60, y0+.15/60, '0.25 m/s', color = 'w', fontsize=6, transform=ccrs.PlateCarree())
+    ax.set_title('SST, ' + day_str + ', surface currents averaged over +/- ' + f'{time_window/2:.1f}' + ' hr', size = 10.)
+    
+    return(ax,startTimeDT,endTimeDT,day_str2)
+
